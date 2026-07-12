a2b6e27d4f028012ee50cfaef75d5841c973b3fb
RRASS05 <113114052+RRASS05@users.noreply.github.com>
2026-07-12 14:02:38 -0300

    verificações extras
    vericação para não receber KEYS vazias em config e database.

--- back/app/core/database.py
+++ back/app/core/database.py
@@ -2,6 +2,9 @@ from app.core.config import DATABASE_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não encontrada no arquivo .env")
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
--- back/app/core/security.py
+++ back/app/core/security.py
@@ -1,7 +1,13 @@
 from app.core.config import ALGORITHM, SECRET_KEY
 from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+
+import jwt
 
 password_hash = PasswordHash.recommended()
+
+if SECRET_KEY is None:
+    raise ValueError("SECRET_KEY não configurada no ambiente")
 
 
 def gerar_hash(senha):
@@ -10,11 +16,6 @@ def gerar_hash(senha):
 
 def verificar_senha(senha, hash):
     return password_hash.verify(senha, hash)
-
-
-from datetime import datetime, timedelta, timezone
-
-import jwt
 
 
 def criar_access_token(data: dict):
@@ -26,4 +27,4 @@ def criar_access_token(data: dict):
 
 
 def verificar_access_token(token: str):
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return jwt.decode(token, SECRET_KEY,algorithms=[ALGORITHM])
