bd65a656167da9963f70ccdeb3103fe0ad0ea6a1
RRASS05 <113114052+RRASS05@users.noreply.github.com>
2026-08-01 20:20:00 -0300

    fix: correção para adicionar varias fotos

--- back/app/crud/foto_crud.py
+++ back/app/crud/foto_crud.py
@@ -1,41 +1,37 @@
+from fastapi import Path
+
 from app.models.foto import Foto
-from app.services.arquivo_service import UPLOADS_DIR
-from pathlib import Path
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 
-def adicionarFoto(db: Session, id_sala: int, caminho: str) -> None:
-    foto = Foto(id_sala=id_sala, caminho=caminho)
+def adicionarFoto(db : Session, id_sala: int, caminho: str):
+    foto = Foto(id_sala = id_sala, caminho = caminho)
     db.add(foto)
-    db.flush()
+    db.flush
 
-
-def buscarFoto(db: Session, id: int) -> Foto | None:
-    return db.scalar(select(Foto).where(Foto.id == id))
-
-
-def removerFoto(db: Session, id_foto: int) -> Foto | None:
-    foto = db.scalar(select(Foto).where(Foto.id == id_foto))
-    if not foto:
-        return None
+def buscarFoto(db : Session, id : int):
+    return db.query(Foto).filter(Foto.id == id).first()
+    
+def removerFoto(db: Session, id_foto:int):
+    foto = db.query(Foto).filter(Foto.id == id_foto).first()
     removerArquivo(db, id_foto)
     db.delete(foto)
-    return foto
 
-
-def removerFotosCompletas(db: Session, id_sala: int) -> None:
-    lista_fotos = list(db.scalars(select(Foto).where(Foto.id_sala == id_sala)))
-
+def removerFotosCompletas(db: Session, id_sala: int):
+    
+    lista_fotos = db.query(Foto).filter(Foto.id_sala == id_sala).all()  
+    
     for foto in lista_fotos:
         removerArquivo(db, foto.id)
         db.delete(foto)
-
-
-def removerArquivo(db: Session, id_foto: int) -> None:
+    
+def removerArquivo(db : Session, id_foto : int):
     foto = buscarFoto(db, id_foto)
-
+    
     if foto:
-        arquivo = UPLOADS_DIR / Path(foto.caminho).name
+        arquivo = Path(foto.caminho)
         if arquivo.exists():
             arquivo.unlink()
+    
+    
+    
--- back/app/models/sala.py
+++ back/app/models/sala.py
@@ -56,7 +56,7 @@ class Sala(Base):
 
     proprietario = relationship("Proprietario", back_populates="salas")
 
-    endereco = relationship("Endereco", back_populates="sala")
+    endereco = relationship("Endereco", back_populates="salas")
     
 
     contratos: Mapped[list["Contrato"]] = relationship(
